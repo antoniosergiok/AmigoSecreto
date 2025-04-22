7cf07d241537d933e6aa4f8c0516b4718a010566
Antonio Sergio Castro de Carvalho Junior <antoniosergiok@gmail.com>
2025-04-22 01:34:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Função para adicionar amigos à lista
-// Testando GitHub
 function adicionarAmigo() {
     let amigo = document.getElementById('amigo');
     let lista = document.getElementById('listaAmigos');
